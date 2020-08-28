3b4ed92e31c2aa3a54bf624497c1ebd2738d15a6
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-08-28 10:23:02 +0200

    Extend categories

---
+++ categories.js
@@ -0,0 +1,7 @@
+const categories = {
+  Computers: ["Monitor", "Graphics Card", "CPU"],
+  Electronics: ["Dishwasher", "Microwave", "Vacuum cleaner"],
+  Televisions: ["Smart TV", "Dumb TV", "4K TV"],
+};
+
+export { categories };
